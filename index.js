5c8e17d2e7b688fe234e28c167c327f0691c7e63
Hassan Ahmed <somii@hotmail.com>
2017-10-24 15:27:05 +0000

    Testing HeroKu

--- index.js
+++ index.js
@@ -5,16 +5,18 @@
     var path = require('path');
     var strftime = require('strftime');
 
-    var app = express();
+    var myApp = express();
     
     var inputTime;
     var convertedTime = {unixTime: null, naturalTime: null};
+    
+    myApp.use('/', express.static(path.join(__dirname, 'public')));
 
 
 
 
 
-app.set('port', (process.env.PORT || 5000));
+myApp.set('port', (process.env.PORT || 5000));
 
 //app.use(express.static(__dirname + '/public'));
 
@@ -22,11 +24,11 @@ app.set('port', (process.env.PORT || 5000));
 //app.set('views', __dirname + '/views');
 //app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+myApp.get('/', function(request, response) {
 //  response.render('pages/index');
 response.end("Hello World 3");
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+myApp.listen(myApp.get('port'), function() {
+  console.log('Node app is running on port', myApp.get('port'));
 });
